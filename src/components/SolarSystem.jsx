c134371d44738fd1d5e3d866e59658b32699de5d
Ikarosv <ikarovieiraa@gmail.com>
2022-10-21 15:33:24 -0300

    feat: Adiciona o componente PlanetCard ao SolarSystem
    Adicionado o componnent PlanetCard ao SolarSystem, usando um .map em planets que foi importado de data/planets

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 import Title from './Title';
 
 export default class SolarSystem extends Component {
@@ -6,6 +8,15 @@ export default class SolarSystem extends Component {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <ul>
+          {
+            planets.map((planet) => (
+              <li key={ planet.name }>
+                <PlanetCard planetName={ planet.name } planetImage={ planet.image } />
+              </li>
+            ))
+          }
+        </ul>
       </div>
     );
   }
