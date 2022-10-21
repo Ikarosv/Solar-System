3bc5062ea90e47a0a19ca62efc775c5756d945ba
Ikarosv <ikarovieiraa@gmail.com>
2022-10-21 16:00:26 -0300

    feat: Adicionado uma lista que renderiza o MissionCard em cada li

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 import Title from './Title';
 
 export default class Missions extends Component {
@@ -6,6 +8,18 @@ export default class Missions extends Component {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <ul>
+          {missions.map(({ name, year, country, destination }) => (
+            <li key={ `mission-${name}` }>
+              <MissionCard
+                name={ name }
+                year={ year }
+                country={ country }
+                destination={ destination }
+              />
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
