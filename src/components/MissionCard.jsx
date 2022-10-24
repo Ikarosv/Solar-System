706ad6961ec41f00c2c13d339acc7677742367b7
Ikarosv <ikarovieiraa@gmail.com>
2022-10-24 17:59:58 -0300

    feat: adicionado nova opção
    quando passar o mouse por cima dos planetas o planeta que o mouse estiver em cima ficará um pouco maior, e todos os outros ficarão cinzas.

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -1,6 +1,9 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import '../assets/styles/MissionCard.css';
+import pin from '../images/Vector-1.svg';
+import flag from '../images/Vector-2.svg';
+import calendar from '../images/Vector.svg';
 
 export default class MissionCard extends Component {
   render() {
@@ -8,9 +11,16 @@ export default class MissionCard extends Component {
     return (
       <div className="card" data-testid="mission-card">
         <p className="name" data-testid="mission-name">{ name }</p>
-        <p className="year" data-testid="mission-year">{ year }</p>
-        <p className="country" data-testid="mission-country">{ country }</p>
-        <p className="destination" data-testid="mission-destination">{ destination }</p>
+        <div className="informations">
+          <div className="yearCountry">
+            <img src={ calendar } alt="" />
+            <p className="year" data-testid="mission-year">{ year }</p>
+            <img src={ pin } alt="" />
+            <p className="country" data-testid="mission-country">{ country }</p>
+          </div>
+          <img src={ flag } alt="" />
+          <p className="destination" data-testid="mission-destination">{ destination }</p>
+        </div>
       </div>
     );
   }
--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -1,7 +1,47 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import '../assets/styles/PlanetCard.css';
 
 export default class PlanetCard extends Component {
+  handleMouseEnter = (event) => {
+    this.addClass(document.querySelectorAll('.planet'), 'dark');
+    this.removeClass([event.target], 'dark');
+    const hovereds = document.querySelectorAll('.hovered');
+    if (hovereds) this.removeClass(hovereds, 'hovered');
+    this.addClass([event.target], 'hovered');
+    this.showName();
+  };
+
+  handleMouseLeave = () => {
+    this.removeClass(document.querySelectorAll('.planet'), 'dark');
+    this.removeClass(document.querySelectorAll('.planet'), 'hovered');
+    this.hideName();
+  };
+
+  showName = () => {
+    const { planetName } = this.props;
+    document.getElementById(`planet${planetName}`)
+      .style.visibility = 'visible';
+  };
+
+  hideName() {
+    const { planetName } = this.props;
+    document.getElementById(`planet${planetName}`)
+      .style.visibility = 'hidden';
+  }
+
+  addClass(elements, className) {
+    elements.forEach((element) => {
+      element.classList.add(className);
+    });
+  }
+
+  removeClass(elements, className) {
+    elements.forEach((element) => {
+      element.classList.remove(className);
+    });
+  }
+
   render() {
     const { planetName, planetImage } = this.props;
     return (
@@ -10,8 +50,17 @@ export default class PlanetCard extends Component {
           src={ planetImage.image }
           width={ planetImage.width }
           alt={ `Planeta ${planetName}` }
+          onMouseEnter={ this.handleMouseEnter }
+          onMouseLeave={ this.handleMouseLeave }
+          className="planet"
         />
-        <p data-testid="planet-name">{ planetName }</p>
+        <p
+          data-testid="planet-name"
+          className="planetName"
+          id={ `planet${planetName}` }
+        >
+          { planetName }
+        </p>
       </div>
     );
   }
@@ -24,9 +73,3 @@ PlanetCard.propTypes = {
     width: PropTypes.string,
   }),
 }.isRequired;
-
-PlanetCard.defaultProps = {
-  planetImage: {
-    width: 'auto',
-  },
-};
